2c18c77d02871b7cec1deabe5db3c1e39de3065c
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-15 21:53:15 -0700

    code for mainCtrl

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,14 +2,39 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+  $scope.getMessages = function() {
+    messageService.getData()
+      .then(function(response) {
 
+        $scope.messages = response.data.chats.reverse();
+        // console.log($scope.messages);
+      });
+  };
 
-
+  $scope.getMessages();
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
+  $scope.postMessage = function(message) {
+    messageSerbice.postMessage(message).then(function(response) {});
+    $scope.message = '';
+  };
 
+  // $scope.getCookie = function(){
+  //    messageService.getCookie()
+  //    .then(function(response) {
+  //     console.log(response.data);
+  //    });
+  // };
+
+  // $scope.getCookie();
+  // $scope.postCookie = function(cookieName) {
+  //  console.log(cookieName);
+  //  messageService.postCookie(cookieName).then(function(response){});
+  // };
+
+  // $scope.postCookie();
 
 
   //uncomment this code when your getMessages function is finished
